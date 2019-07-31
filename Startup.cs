15a614c99fd6e50bedc60b070f24ea7412f6c065
Sabit Igde <sabitertan@gmail.com>
2019-07-30 20:38:22 -0400

    first commit

---
+++ Startup.cs
@@ -0,0 +1,19 @@
+using BionicExtensions.Attributes;
+using Microsoft.AspNetCore.Components.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BionicWars
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            InjectableAttribute.RegisterInjectables(services);
+        }
+
+        public void Configure(IComponentsApplicationBuilder app)
+        {
+            app.AddComponent<App>("app");
+        }
+    }
+}
